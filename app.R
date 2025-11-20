fe6b3e50bc43df30d92e151f62e07f1d78531c81
aramir21 <aramir21@eafit.edu.co>
2025-11-19 22:34:03 -0500

    New App.R

--- app.R
+++ app.R
@@ -1,40 +1,38 @@
 
 
-dep=c("rsconnect" ,"shiny","mlogit" ,"MASS" ,"AER" ,
+dep <- c("rsconnect" ,"shiny","mlogit" ,"MASS" ,"AER" ,
       "mcmc","bayesm",
       "abind","bayesboot","bibtex" ,
       "BMA","car","carData","cellranger" ,"clipr",
-      "coda","DT","Formula","ivbma",
+      "coda","DT","Formula",
       "Matrix" ,"matrixcalc","RcppEigen",
       "Rdpack","rhandsontable" ,"statmod", "truncnorm"  ,
-      "stringr","MCMCpack","dma",'dlm',"bvartools","stochvol","bayesforecast","ggpubr","latex2exp")
+      "stringr","dma",'dlm',"bvartools","stochvol","bayesforecast","ggpubr","latex2exp")
 
+special <- c("ivbma", "MCMCpack")
 
+ip <- rownames(installed.packages())
 
-needed=dep[!dep%in%installed.packages()]
+## 1) Instalar TODOS los demás desde CRAN
+needed <- dep[!dep %in% ip]
 
-if (length(needed)>0){
-  for (p in needed){
-    install.packages(p)
-  }
+if (length(needed) > 0) {
+  install.packages(needed)
 }
 
-needed=dep[!dep%in%installed.packages()]
-
-
-if (length(needed)>0){
-  for (p in needed){
-    if (p=="ivbma"){
-      install.packages("https://cran.r-project.org/src/contrib/Archive/ivbma/ivbma_1.05.tar.gz", repos = NULL, type = "source")
-    }else if (p=="MCMCpack"){
-      # install.packages("https://cran.r-project.org/src/contrib/Archive/MCMCpack/MCMCpack_1.4-4.tar.gz", repos = NULL, type = "source")
-      install.packages("https://github.com/BESmarter-consultancy/BSTApp/raw/master/MCMCpack_1.7-1.tar.gz", repos = NULL, type = "source")
-    }else{
-      install.packages(p)
-    }
-  }
+## 2) Instalar ivbma desde el tarball
+if (!"ivbma" %in% ip) {
+  install.packages(
+    "https://cran.r-project.org/src/contrib/Archive/ivbma/ivbma_1.05.tar.gz",
+    repos = NULL, type = "source"
+  )
 }
 
+## 3) Instalar SIEMPRE versión corregida de MCMCpack
+install.packages(
+  "https://github.com/BESmarter-consultancy/BSTApp/raw/master/MCMCpack_1.7-1.tar.gz",
+  repos = NULL, type = "source"
+)
 
 
 library(shiny)
